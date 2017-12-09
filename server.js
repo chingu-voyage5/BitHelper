05174fc5579f4c1a216dae34144302cbd6244c74
shibatas <shohei51@gmail.com>
2017-12-09 05:06:35 +0100

    testing api url

--- server.js
+++ server.js
@@ -23,6 +23,18 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  //and remove cacheing so we get the most recent comments
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
 // Serve static assets
 app.use(express.static(path.resolve('build')));
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import CommentBox from './CommentBox';
 
 require('dotenv').load();
-console.log(process.env);
 
 const apiUrl = process.env.REACT_APP_APIURL;
+const apiUrlTest = window.location.origin + '/api';
 
+axios.get(apiUrl)
+.then(res => {
+  console.log(res);
+})
+
+axios.get(apiUrlTest)
+.then(res => {
+  console.log(res);
+})
+/*
 ReactDOM.render(<CommentBox 
     url={apiUrl + '/comments'}
     pollInterval={2000} />, document.getElementById('root'));
+*/
