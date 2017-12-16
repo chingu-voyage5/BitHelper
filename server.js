3f73cf684ac006f68949e56a7a2b8f0c28e3ad27
shibatas <shohei51@gmail.com>
2017-12-16 18:29:55 +0100

    changed callback URL to heroku

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ routes(router);
 
 // auth
 passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    clientID: process.env.GITHUB_CLIENT_ID_HEROKU,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET_HEROKU,
     callbackURL: "/auth/github/callback",
     proxy: true
   },
