52d3c640ef3280d1b4602cf10fa7abdab88f80fc
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-12-29 16:40:38 +0100

    Added bootstrap classes to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,23 @@ class Header extends Component {
         super(props)
 
     }
-
-
     render () {
       return (
         <header>
-          <div className='title'>Bears-20 App</div>
-          <Auth user={this.props.user} logoutUser={this.props.logoutUser}/>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Bears-20 App</a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Auth className="nav-link" user={this.props.user} logoutUser={this.props.logoutUser}/>
+                  </li>
+
+                </ul>
+            </div>
+          </nav>         
         </header>
       );
     }
