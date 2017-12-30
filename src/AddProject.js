ca5e30589257c6013cd6b95407f11c21850c4ad6
marckimbrey <marckimbrey@gmail.com>
2017-12-30 14:38:52 +0000

    Basic createPoll form component

---
+++ src/AddProject.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './style.css';
+
+// const ProjectsSchema = new Schema({
+//     id: String,
+//     title: String,        //title of the project
+//     owner: String,        //username of the post creator
+//     description: String,  //project description
+//     status: String,       //status of project, why it's stuck
+//     repoUrl: String,      //GitHub repo URL
+//     img: [String]         //image URLs of screenshots
+// });
+
+
+class AddProject extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          title: "",
+          owner: props.user.displayName,
+          description: "",
+          status: "",
+          repoUrl: "",
+          img: []
+        }
+
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this)
+    }
+
+    onInputChange(event) {
+      const newValue = {}
+      newValue[event.target.name] = event.target.value;
+
+      this.setState(...this.state, newValue);
+
+    }
+    onFormSubmit(e) {
+      e.preventDefault()
+      console.log(this.props)
+      this.props.createPoll(this.state)
+    }
+    render() {
+
+         return (
+           <div>
+             <form onSubmit={this.onFormSubmit}>
+             <fieldset>
+
+
+             <legend>add-project</legend>
+
+
+             <div class="form-group">
+               <label class="control-label" for="title">Title</label>
+               <input
+                 name="title"
+                 type="text"
+                 placeholder="project title"
+                 class="form-control input-md"
+                 value={this.state.title}
+                 onChange={this.onInputChange} required="" />
+
+             </div>
+
+
+             <div class="form-group">
+                <label class="control-label" for="description">Description</label>
+                <textarea
+                  class="form-control"
+                  name="description"
+                  value={this.state.description}
+                  placeholder="Project description"
+                  onChange={this.onInputChange}
+                />
+             </div>
+
+
+             <div class="form-group">
+                <label class="control-label" for="status">Project Status</label>
+                <textarea
+                  class="form-control"
+                  name="status"
+                  value={this.state.status}
+                  placeholder="Current state of the project, reason project needs help"
+                  onChange={this.onInputChange}
+                  required="" />
+             </div>
+
+
+             <div class="form-group">
+                <label class="control-label" for="repoUrl">Github repo</label>
+                <input
+                  name="repoUrl"
+                  value={this.state.repoUrl}
+                  type="search"
+                  placeholder="link to github repo"
+                  class="form-control input-md"
+                  onChange={this.onInputChange}
+                  required="" />
+             </div>
+
+
+             <div class="form-group">
+               <label class="col-md-4 control-label" for="img">Img</label>
+               <input
+                 name="img"
+                 type="text"
+                 value={this.state.img}
+                 placeholder="Links to project screenshots, mockups "
+                 class="form-control input-md"
+                 onChange={this.onInputChange}
+               />
+             </div>
+
+             <button type="submit" >Submit</button>
+
+             </fieldset>
+             </form>
+             <button id='backToList' onClick={() => this.props.history.push('/')}>Back to Main</button>
+         </div>
+        );
+    }
+}
+
+export default AddProject;
