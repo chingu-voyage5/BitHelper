626587be622ad38ac19381445295230d905c2d61
Shohei Shibata <shohei51@gmail.com>
2018-01-13 21:41:03 +0100

    addProject page working, except the AuthenticatedUser wrapper.

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './style.css';
+import '../stylesheets/components/addProject.css';
+//import './style.css';
 
 // const ProjectsSchema = new Schema({
 //     id: String,
@@ -16,10 +17,10 @@ import './style.css';
 class AddProject extends Component {
     constructor(props) {
         super(props);
-
+        console.log(props);
         this.state = {
           title: "",
-          owner: props.user.displayName,
+          owner: "",
           description: "",
           status: "",
           repoUrl: "",
@@ -29,7 +30,19 @@ class AddProject extends Component {
         this.onInputChange = this.onInputChange.bind(this);
         this.onFormSubmit = this.onFormSubmit.bind(this)
     }
-
+    componentDidMount() {
+      // if user is not logged in and therefore user info is null, redirect to home
+      // redirect to login page in the future
+      if (!this.props.user) {
+          setTimeout(() => {
+              this.props.history.push('/');
+          }, 3000);
+      } else {
+        this.setState({
+          owner: this.props.user.displayName
+        });
+      }
+    }
     onInputChange(event) {
       const newValue = {}
       newValue[event.target.name] = event.target.value;
@@ -43,85 +56,95 @@ class AddProject extends Component {
       this.props.createPoll(this.state)
     }
     render() {
-
+      if (!this.props.user) {
+          return <h3>ERROR: Not logged in. Redirecting...</h3>;
+      } else {
          return (
-           <div>
-             <form onSubmit={this.onFormSubmit}>
-             <fieldset>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <div className="card">
+                <form onSubmit={this.onFormSubmit}>
+                <fieldset>
 
 
-             <legend>add-project</legend>
+                <legend>add-project</legend>
 
 
-             <div class="form-group">
-               <label class="control-label" for="title">Title</label>
-               <input
-                 name="title"
-                 type="text"
-                 placeholder="project title"
-                 class="form-control input-md"
-                 value={this.state.title}
-                 onChange={this.onInputChange} required="" />
+                <div class="form-group">
+                  <label class="control-label" for="title">Title</label>
+                  <input
+                    name="title"
+                    type="text"
+                    placeholder="project title"
+                    class="form-control input-md"
+                    value={this.state.title}
+                    onChange={this.onInputChange} required="" />
 
-             </div>
-
-
-             <div class="form-group">
-                <label class="control-label" for="description">Description</label>
-                <textarea
-                  class="form-control"
-                  name="description"
-                  value={this.state.description}
-                  placeholder="Project description"
-                  onChange={this.onInputChange}
-                />
-             </div>
+                </div>
 
 
-             <div class="form-group">
-                <label class="control-label" for="status">Project Status</label>
-                <textarea
-                  class="form-control"
-                  name="status"
-                  value={this.state.status}
-                  placeholder="Current state of the project, reason project needs help"
-                  onChange={this.onInputChange}
-                  required="" />
-             </div>
+                <div class="form-group">
+                    <label class="control-label" for="description">Description</label>
+                    <textarea
+                      class="form-control"
+                      name="description"
+                      value={this.state.description}
+                      placeholder="Project description"
+                      onChange={this.onInputChange}
+                    />
+                </div>
 
 
-             <div class="form-group">
-                <label class="control-label" for="repoUrl">Github repo</label>
-                <input
-                  name="repoUrl"
-                  value={this.state.repoUrl}
-                  type="search"
-                  placeholder="link to github repo"
-                  class="form-control input-md"
-                  onChange={this.onInputChange}
-                  required="" />
-             </div>
+                <div class="form-group">
+                    <label class="control-label" for="status">Project Status</label>
+                    <textarea
+                      class="form-control"
+                      name="status"
+                      value={this.state.status}
+                      placeholder="Current state of the project, reason project needs help"
+                      onChange={this.onInputChange}
+                      required="" />
+                </div>
 
 
-             <div class="form-group">
-               <label class="col-md-4 control-label" for="img">Img</label>
-               <input
-                 name="img"
-                 type="text"
-                 value={this.state.img}
-                 placeholder="Links to project screenshots, mockups "
-                 class="form-control input-md"
-                 onChange={this.onInputChange}
-               />
-             </div>
+                <div class="form-group">
+                    <label class="control-label" for="repoUrl">Github repo</label>
+                    <input
+                      name="repoUrl"
+                      value={this.state.repoUrl}
+                      type="search"
+                      placeholder="link to github repo"
+                      class="form-control input-md"
+                      onChange={this.onInputChange}
+                      required="" />
+                </div>
 
-             <button type="submit" >Submit</button>
 
-             </fieldset>
-             </form>
-             <button id='backToList' onClick={() => this.props.history.push('/')}>Back to Main</button>
-         </div>
+                <div class="form-group">
+                  <label class="col-md-4 control-label" for="img">Img</label>
+                  <input
+                    name="img"
+                    type="text"
+                    value={this.state.img}
+                    placeholder="Links to project screenshots, mockups "
+                    class="form-control input-md"
+                    onChange={this.onInputChange}
+                  />
+                </div>
+
+                <button type="submit" >Submit</button>
+
+                </fieldset>
+                </form>
+                <button id='backToList' onClick={() => this.props.history.push('/')}>Back to Main</button>
+              </div>
+            </div>
+          </div>
+        </div>
         );
+    
+      }
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -48,8 +48,16 @@ class App extends Component {
       console.error('local server not running. using heroku deployment of the server instead.');
       url = process.env.REACT_APP_APPURL;
     });
+
+    axios.get(url + '/auth')
+    .then(res => {
+      this.setState({
+        user: res.data,
+        isLoggedIn: true
+      });
+    })
     // if userId is stored in cookie get user
-    const userId = Cookies.get("userId");
+    /*const userId = Cookies.get("userId");
     if(userId) {
       console.log('cookie userId', userId);
 
@@ -60,7 +68,7 @@ class App extends Component {
           isLoggedIn: true
         })
       })
-    }
+    }*/
   }
   postUser = (data) => {
     console.log('post user', data);
@@ -98,29 +106,64 @@ class App extends Component {
     return(
     <Router>
       <div>
-         <Header user={this.state.user} logoutUser={this.logoutUser}/>
-          <Route exact
-            path="/"
-            render={(routeProps)=> {
-            return <ProjectCard {...routeProps} {...this.state}  />
+        <Header user={this.state.user} logoutUser={this.logoutUser}/>
+        <Route exact
+          path="/"
+          render={(routeProps)=> {
+          return <ProjectCard {...routeProps} {...this.state}  />
+        }
+        }/>
+        <Route path="/project/:id" render={(routeProps)=> {
+          return <ProjectInfo {...routeProps} {...this.state} />
+        }
+        }/>
+        <Route path="/user/:id" render={(routeProps)=> {
+          return <UserInfo {...routeProps} {...{user: this.state.user}} />
+        }
+        }/>
+        <Route path="/editUser" render={(routeProps)=> {
+          return <UserEdit {...routeProps} {...{
+            user: this.state.user,
+            onUserPost: this.postUser
+          }} />
+        }
+        }/>
+        <Route path="/addproject" render={(routeProps)=> {
+              return <AddProject
+                {...routeProps}
+                {...{
+                  user: this.state.user,
+                  createPoll: this.createPoll
+                }} />
           }
-         }/>
-         <Route path="/project/:id" render={(routeProps)=> {
-           return <ProjectInfo {...routeProps} {...this.state} />
-          }
-         }/>
-         <Route path="/user/:id" render={(routeProps)=> {
-           return <UserInfo {...routeProps} {...{user: this.state.user}} />
-          }
-         }/>
-         <Route path="/editUser" render={(routeProps)=> {
-           return <UserEdit {...routeProps} {...{
-              user: this.state.user,
-              onUserPost: this.postUser
-            }} />
-          }
-         }/>
-         // Wrapper component that only renders routes when user islogged in
+        }/>
+
+      </div>
+     </Router>
+   )
+ }
+
+}
+
+// wrapper function for protected routes
+const AuthenticatedUser = (prop) =>  {
+    console.log('AuthenticatedUser', prop);
+    /*if (user) {
+      // return protected routes
+      return <div>{children}</div>
+    } else {
+      console.log('user not logged in')
+      // todo: should redirect to a login page
+      return null
+    }*/
+}
+
+ReactDOM.render(<App
+/>, document.getElementById('root'));
+
+/*
+
+// Wrapper component that only renders routes when user islogged in
          <AuthenticatedUser isLoggedin={this.state.user}>
            <Route path="/addproject" render={(routeProps)=> {
                 return <AddProject
@@ -132,27 +175,4 @@ class App extends Component {
             }
           }/>
         </AuthenticatedUser>
-
-
-      </div>
-     </Router>
-   )
- }
-
-}
-
-// wrapper function for protected routes
-const AuthenticatedUser = ({user, children}) =>  {
-
-    if (user) {
-      // return protected routes
-      return <div>{children}</div>
-    } else {
-      console.log('user not logged in')
-      // todo: should redirect to a login page
-      return null
-    }
-}
-
-ReactDOM.render(<App
-/>, document.getElementById('root'));
+*/
