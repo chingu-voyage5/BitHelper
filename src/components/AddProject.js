694c26f394dccfdc226ee1e60ba51133d3b4314c
Shohei Shibata <shohei51@gmail.com>
2018-01-16 23:00:38 +0100

    Add project form updated for formatting and use of separate Input component

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // import '../stylesheets/components/addProject.css';
 import '../stylesheets/main.css'; // for dev
 import Button from './Button.js';
+import Input from './Input'
 
 // const ProjectsSchema = new Schema({
 //   id: String, 
@@ -20,7 +21,6 @@ import Button from './Button.js';
 class AddProject extends Component {
     constructor(props) {
         super(props);
-        console.log(props);
         this.state = {
           title: "",
           owner: "",
@@ -30,10 +30,7 @@ class AddProject extends Component {
           status: "",
           repoUrl: "",
           img: []
-        }
-
-        this.onInputChange = this.onInputChange.bind(this);
-        this.onFormSubmit = this.onFormSubmit.bind(this)
+        };
     }
     componentDidMount() {
       // if user is not logged in and therefore user info is null, redirect to home
@@ -48,121 +45,84 @@ class AddProject extends Component {
         });
       }
     }
-    onInputChange(event) {
-      const newValue = {}
-      newValue[event.target.name] = event.target.value;
-
+    onInputChange = (name, value) => {
+      const newValue = {};
+      newValue[name] = value;
       this.setState(...this.state, newValue);
-
     }
-    onFormSubmit(e) {
+    onFormSubmit = (e) => {
       e.preventDefault();
-      if (this.state.title.length > 0) {
-        this.props.createPoll(this.state)
-      } else {
-        alert('Please at least enter a title!');
-      }
+      this.props.createPoll(this.state);
+      this.props.history.push('/');
     }
     render() {
       if (!this.props.user) {
           return <h3>ERROR: Not logged in. Redirecting...</h3>;
       } else {
-         return (
+        let inputFields = [
+          {
+            label: 'Project Title',
+            name: 'title',
+            placeholder: 'e.g. the Ninja project',
+            required: true
+          },
+          {
+            label: 'Category',
+            name: 'title',
+            type: 'text',
+            placeholder: 'e.g. Social, Games, Productivity, etc.'
+          },
+          {
+            label: 'Description',
+            tag: 'textarea',
+            name: 'description',
+            placeholder: 'e.g. This is the coolest project ever',
+            required: true
+          },
+          {
+            label: 'Project Status',
+            tag: 'textarea',
+            name: 'status',
+            placeholder: 'e.g. Explain what is the current state of the project, why you need help and what roles you might need',
+            required: true
+          },
+          {
+            label: 'Stack',
+            name: 'stack',
+            type: 'text',
+            placeholder: 'Languages, frameworks, libraries... separate by comma'
+          },
+          {
+            label: 'Code Repository',
+            name: 'repoUrl',
+            placeholder: 'http://github.com/username/github-repo',
+            required: true
+          },
+          {
+            label: 'Image',
+            name: 'img',
+            placeholder: 'e.g. http://via.placeholder.com/400x300'
+          }
+        ];
+        return (
           <div className="container">
             <div className="row">
               <div className="col">
                 <div className="material-card">
-                <h1>Add a project </h1>
-                <form onSubmit={this.onFormSubmit}>
-                  <fieldset>
-                    <div className="form-group">
-                      <label className="control-label" for="title">Project title</label>
-                      <input
-                        name="title"
-                        type="text"
-                        placeholder="e.g. the Ninja project"
-                        className="form-control input-md"
-                        value={this.state.title}
-                        onChange={this.onInputChange} required="" />
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label" for="category">Category</label>
-                      <input
-                        name="category"
-                        type="text"
-                        placeholder="category"
-                        className="form-control input-md"
-                        value={this.state.category}
-                        onChange={this.onInputChange} required="" />
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label" for="description">
-                        Description</label>
-                      <textarea
-                        className="form-control"
-                        name="e.g. This is the coolest project ever"
-                        value={this.state.description}
-                        placeholder="Project description"
-                        onChange={this.onInputChange}
-                      />
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label" for="status"><strong>Project Status</strong></label>
-                      <textarea
-                        className="form-control"
-                        name="status"
-                        value={this.state.status}
-                        placeholder="E.g. Explain what is the current state of the project, why you need help and what roles you might need"
-                        onChange={this.onInputChange}
-                        required="" />
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label" for="stack">Stack</label>
-                      <input
-                        name="stack"
-                        type="text"
-                        placeholder="Languages, frameworks, libraries... separate by comma"
-                        className="form-control input-md"
-                        value={this.state.stack}
-                        onChange={this.onInputChange} required="" />
-                    </div>
-
-                    <div className="form-group">
-                      <label className="control-label" for="repoUrl">
-                        Code repository</label>
-                      <input
-                        name="repoUrl"
-                        value={this.state.repoUrl}
-                        type="search"
-                        placeholder="http://github.com/username/github-repo"
-                        className="form-control input-md"
-                        onChange={this.onInputChange}
-                        required="" />
-                    </div>
-
-                    <div className="form-group">
-                        <label className="col-md-4 control-label" for="img">Image</label>
-                        <input
-                          name="img"
-                          type="text"
-                          value={this.state.img}
-                          placeholder="e.g. http://via.placeholder.com/400x300"
-                          className="form-control input-md"
-                          onChange={this.onInputChange}
-                        />
-                    </div>
-
-                  <button className="btn" type="submit" onClick={() => this.props.history.push('/')}>Submit</button>
-
-                  </fieldset>
-                </form>
-                <Button label="Back to home" />
-              </div>
-              <button className='btn btn-primary' onClick={() => this.props.history.push('/')}>Back to Home</button>
+                  <h1>Add a project </h1>
+                  <form onSubmit={this.onFormSubmit}>
+                    <fieldset>
+                      {inputFields.map(item => {
+                        return <Input onChange={this.onInputChange} data={item}/>;
+                      })}
+                      <div className='btn-container'>
+                        <input type='submit' className='btn' value='Submit' />
+                        <input type='reset' className='btn' value='Reset' />'
+                      </div>
+                    </fieldset>
+                  </form>
+                </div>
+              <Button label="Back to home" />
             </div>
           </div>
         </div>
---
+++ src/components/Input.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import '../stylesheets/main.css'; // for dev
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    }
+  }
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+    this.props.onChange(e.target.name, e.target.value);
+  }
+  render(){
+    const data = Object.assign({}, this.props.data);
+    switch (this.props.data.tag) {
+      case ('textarea'):
+        return (
+          <div className="form-group">
+            <label className="control-label" for={data.name}>{data.label}</label>
+            <textarea
+              id={data.name}
+              name={data.name}
+              type={data.type ? data.type : 'text'}
+              placeholder={data.placeholder}
+              className="form-control input-md"
+              onChange={this.onChange}
+              required={data.required} 
+            />
+          </div>
+        );
+        break;
+      default: 
+        return (
+          <div className="form-group">
+            <label className="control-label" for={data.name}>{data.label}</label>
+            <input
+              id={data.name}
+              name={data.name}
+              type={data.type ? data.type : 'text'}
+              placeholder={data.placeholder}
+              className="form-control input-md"
+              onChange={this.onChange}
+              required={data.required ? data.required : false} 
+            />
+          </div>
+        );        
+    }
+  }
+}
+
+export default Input;
