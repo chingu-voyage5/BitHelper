ee0ca68a9b1ba7503d54199d9b9c3988e3333879
Shohei <shohei51@gmail.com>
2018-02-06 08:29:49 +0000

    Cleaned up and commented: server.js

--- server.js
+++ server.js
@@ -12,49 +12,49 @@ const app = express();
 const router = express.Router();
 const auth = require('./config/auth')
 
+// Load .env variables
 require('dotenv').load();
 
+// passport.js contains the Passport auth strategies. Currently only GitHub
 require('./config/passport')(passport);
 
 const port = process.env.PORT || 3001;
 
-// db config
+// Connect to MongoDB database
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useMongoClient: true })
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// configure body parser for json format
+// Configure body parser for json format
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Initialize Express Session
 app.use(session({
     secret: 'bears-20',
 	resave: false,
 	saveUninitialized: true
 }));
 
-//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
+// Initialize Passport for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set Headers to allow Cross Origin Requests
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  //and remove cacheing so we get the most recent comments
-  res.setHeader('Cache-Control', 'no-cache');
   next();
 });
 
-// Load api routes
-routes(router);
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 // Serve static assets
 app.use(express.static(path.resolve('build')));
+
+// Load api routes
+routes(router);
 
 // Defines api route root
 app.use('/api', router);
