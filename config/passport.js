db1aa47749d50d3babdb69e4864d6c6e5de694d9
Shohei Shibata <shohei51@gmail.com>
2018-02-07 05:20:18 +0100

    Finished commenting entire '/config' folder

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,11 @@
+// All authentication strategy code should go here.
+
 const GitHubStrategy = require('passport-github').Strategy;
 const User = require('../model/users');
 
 module.exports = function(passport) {
 
+    // This middleware is processed for each login call
     passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
@@ -10,14 +13,14 @@ module.exports = function(passport) {
         proxy: true
       },
       function(accessToken, refreshToken, profile, cb) {
-    
+        // Look for the user in existing database
         User.findOne({
             'githubId': profile.id
         }, function(err, user) {
             if (err) {
                 return cb(err);
             }
-            //No user was found... so create a new user with values from github
+            // No user was found... so create a new user with values from github
             if (!user) {
                 user = new User({
                     githubId: profile.id,
@@ -32,17 +35,21 @@ module.exports = function(passport) {
                     return cb(err, user);
                 });
             } else {
-                //found user. Return
+                // Round user. Return
                 return cb(err, user);
             }
         });
       }
     ));
     
+    // User ID is stored in the passport session,
+    // which can be used to access the rest of the User data
     passport.serializeUser(function(user, done) {
       done(null, user.id);
     });
     
+    // deserializeUser returns the entire User data, 
+    // when User ID is provided
     passport.deserializeUser(function(id, done) {
       User.findById(id, function (err, user) {
         done(err, user);
