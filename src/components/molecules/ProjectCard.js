65986d932ea36b2ce511e2e3fee4e1b6915f7da8
Shohei <shohei51@gmail.com>
2018-03-14 20:04:35 +0000

    Eliminated setProject() function from ProjectCard

--- src/components/molecules/ProjectCard.js
+++ src/components/molecules/ProjectCard.js
@@ -13,7 +13,6 @@ import SearchBox from '../molecules/SearchBox';
 class ProjectCard extends Component {
     constructor(props) {
         super(props);
-        console.log('url params', this.props.match);
         this.state = {
           filters: [],
           limit: (this.props.match.path === '/') ? (6) : (null)
@@ -21,6 +20,7 @@ class ProjectCard extends Component {
     }
     // for filtering projects to show, by filter state
     filterProjects = (projects, filters) => {
+        if (projects.length < 1) { return null }
         // if no filter is set, display all projects
         if (!filters || filters.length < 1) {
             return projects;
@@ -39,36 +39,6 @@ class ProjectCard extends Component {
             return projects;   
         }
     }
-    setProjects() {
-        if (this.props.projects.length > 0) {
-            const filters = this.state.filters;
-            const projects = this.filterProjects(this.props.projects, filters);
-            return projects.map((item,i) => {
-                if (!this.state.limit || i < this.state.limit) {
-                    return (
-                        <div className="col-md-3 card"
-                            onClick={() => this.props.history.push('/project/view/' + item._id)}
-                            key={i}
-                            id={item._id}>
-                            <div className="card-body">
-                                <p className="card-category">{item.category}</p>
-                                <h4 className="card-title">{item.title}</h4>
-                                    <Dotdotdot clamp={4}>
-                                    <p className="card-text">
-                                        {item.description}</p>
-                                    </Dotdotdot>
-                                <Button label="More" />
-                            </div>
-                        </div>  
-                    );      
-                } else {
-                    return null;
-                }
-            });
-        } else {
-            return <Loader />
-        }
-    }
     handleFilterUpdate = (filterArray) => {
         this.setState({
           filters: filterArray
@@ -76,28 +46,61 @@ class ProjectCard extends Component {
     }
     render() {
         const partial = Boolean(this.state.limit);
-        return (
-            <div className={(partial) ? 
-                ("container") : 
-                ("container project-cards-full")}
-            >
-                {(!partial) ? (
-                    <SearchBox projects={this.props.projects} filters={this.state.filters} onTagsUpdate={this.handleFilterUpdate}/>
-                ) : (
-                    null
-                )}
-                <div className="row justify-content-center">
-                    {this.setProjects()}
+        let projects = this.filterProjects(this.props.projects, this.state.filters);
+        if (projects) {
+            console.log('projects', projects);
+            return (
+                <div className={partial ? "container" : "container project-cards-full"}
+                >
+                    {(!partial) ? (
+                        <SearchBox projects={this.props.projects} filters={this.state.filters} onTagsUpdate={this.handleFilterUpdate}/>
+                    ) : (
+                        null
+                    )}
+                    <div className="row justify-content-center">
+                        {projects.map((project, i) => {
+                            if (!partial || i < this.state.limit) {
+                                return (
+                                    <Card 
+                                        project={project}
+                                        onClick={() => this.props.history.push('/project/view/' + project._id)}
+                                    />
+                                );
+                            } else {
+                                null
+                            }
+                        })}
+                    </div>
+                    <div className="text-center">
+                        {(partial) ? 
+                            (<Button label="All projects" redirect="/project/view/"/>) :
+                            (<Button label="Back to main" redirect="/" />)
+                        }
+                    </div>
                 </div>
-                <div className="text-center">
-                    {(partial) ? 
-                        (<Button label="All projects" redirect="/project/view/"/>) :
-                        (<Button label="Back to main" redirect="/" />)
-                    }
-                </div>
-            </div>
-        );
+            );
+        } else {
+            return <Loader />
+        }
+        
     }
 }
+
+const Card = ({project, onClick}) => (
+    <div className="col-md-3 card"
+        onClick={onClick}
+        key={project._id}
+        id={project._id}>
+        <div className="card-body">
+            <p className="card-category">{project.category}</p>
+            <h4 className="card-title">{project.title}</h4>
+                <Dotdotdot clamp={4}>
+                <p className="card-text">
+                    {project.description}</p>
+                </Dotdotdot>
+            <Button label="More" />
+        </div>
+    </div>  
+);
 
 export default ProjectCard;
