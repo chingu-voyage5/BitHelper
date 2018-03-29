b2dc19b1bd4fe8f04dd1a79db5e68e4c0577d84d
Shohei Shibata <shohei51@gmail.com>
2018-03-30 07:09:46 +0900

    Created projectStatus.js to return project-user status

---
+++ src/js/projectStatus.js
@@ -0,0 +1,68 @@
+const projectStatus = {
+  // Get status for all projects by user
+  myProjects(projects, userId) {
+    if (!projects) return null;
+    return projects.reduce((result, project) => {
+      if (!project.users) return result;
+      let status = project.users.find(user => {
+        return user._id === userId;
+      });
+      if (status) {
+        result.push([project._id, status]);
+      } 
+      return result;
+    });
+  },
+  // Get status of a user for a certain project
+  getStatusById(projects, projectId, userId) {
+    if (!projects) return null;
+    const project = projects.find(item => {
+      return item._id === projectId;
+    });
+    if (!project) {
+      console.log('projectStatus.getStatusById: Project not found')
+      return null;
+    }
+    if (!project.users) return null;
+    const found = project.users.find(user => {
+      return user._id === userId
+    });
+    return found._id;
+  },
+  // Get owner ID for a project
+  getOwner(projects, projectId, projectId) {
+    if (!projects) return null;
+    const project = projects.find(item => {
+      return item._id === projectId;
+    });
+    if (!project) {
+      console.log('projectStatus.getStatusById: Project not found')
+      return null;
+    }
+    if (!project.users) return null;
+    const found = project.users.find(user => {
+      return user.status === 'owner';
+    });
+    return found._id;
+  },
+  // Get all followers for a project
+  getFollowers(projects, projectId) {
+    if (!projects) return null;
+    const project = projects.find(item => {
+      return item._id === projectId;
+    });
+    if (!project) {
+      console.log('projectStatus.getStatusById: Project not found')
+      return null;
+    }
+    if (!project.users) return null;
+    const followers = project.users.filter(user => {
+      return user.status === 'following';
+    });
+    return followers.map(item => {
+      return item._id;
+    });
+  }
+};
+
+export default projectStatus;
