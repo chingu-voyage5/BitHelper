ef34ac135de9e6d93fd94c550d3094ccdf9490f8
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-06-14 10:41:08 +0100

    Use Bootstrap classes for spacing in footer

--- src/components/molecules/Footer.js
+++ src/components/molecules/Footer.js
@@ -10,21 +10,21 @@ import GoMarkGithub from "react-icons/lib/go/mark-github";
 const Footer = () => {
   return <footer className="bg-dark text-center d-flex align-items-center">
       <div className="container">
-        <div className="row footer-row">
-          <div className="col footer-developers">
-            <p>
+        <div className="row mt-3 mt-md-4 footer-row">
+          <div className="col-md mb-3 mb-md-4 footer-developers">
+            <p className="my-0">
               Developed with <FaHeart /> by
               <a href="https://github.com/shibatas/"> Shibatas</a>,
               <a href="https://github.com/marckimbrey"> MarcKimbrey</a> and
               <a href="https://github.com/alexgherardelli"> AlexGherardelli</a>
             </p>
           </div>
-          <div className="col">
+          <div className="col-md mb-3 mb-md-4">
             <div className="footer-copyrights">
               <a href="https://github.com/chingu-voyage3/bears-20">
                 <GoMarkGithub className="github-logo" />
               </a>
-              <p> A Chingu project. © Copyright {new Date().getFullYear()}</p>
+              <p className="my-0"> A Chingu project. © Copyright {new Date().getFullYear()}</p>
             </div>
           </div>
         </div>
