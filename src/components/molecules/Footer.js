585aa9aab1d7f533858f14150bac15ca408d82d4
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-06-15 14:20:27 +0100

    Give more room to developers list in footer on wider screens

--- src/components/molecules/Footer.js
+++ src/components/molecules/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
   return <footer className="bg-dark text-center d-flex align-items-center">
       <div className="container">
         <div className="row mt-3 mt-md-4 footer-row">
-          <div className="col-md mb-3 mb-md-4 footer-developers">
+          <div className="col-md col-lg-8 mb-3 mb-md-4 d-flex justify-content-center justify-content-md-start align-items-center">
             <p className="my-0 text-md-left">
               Developed with <FaHeart /> by
               <a href="https://github.com/shibatas/"> Shibatas</a>,
@@ -19,7 +19,7 @@ const Footer = () => {
               <a href="https://github.com/alexgherardelli"> AlexGherardelli</a>
             </p>
           </div>
-          <div className="col-md mb-3 mb-md-4 d-flex justify-content-center justify-content-md-end align-items-center">
+          <div className="col-md col-lg-4 mb-3 mb-md-4 d-flex justify-content-center justify-content-md-end align-items-center">
             <a className="github-link" href="https://github.com/chingu-voyage3/bears-20" aria-label="GitHub Repository">
               <GoMarkGithub className="github-logo" />
             </a>
