5aa6c84c4baa6860f87166306927f137d9a43fa2
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-07-13 19:36:47 +0200

    Align project card buttons and adjust spacing between elements
    - use flexbox to ensure buttons are displayed at the bottom of the cards
    - add space between paragraph and button
    - remove all button margins to align them properly
    - add "<no category>" text for projects without a category to avoid
      having a huge empty space above the card title

--- src/components/molecules/ProjectCard.js
+++ src/components/molecules/ProjectCard.js
@@ -24,14 +24,17 @@ const ProjectCard = ({user, project, onClick, onFollow}) => (
                 null
             )
         }
-        <div className="card-body">
-            <p className="card-category">{project.category}</p>
-            <h4 className="card-title">{project.title}</h4>
+        <div className="card-body d-flex flex-column justify-content-between">
+            <div className="mb-4">
+                <p className="card-category">{project.category || "<no category>"}</p>
+                <h4 className="card-title">{project.title}</h4>
                 <Dotdotdot clamp={4}>
-                <p className="card-text">
-                    {project.description}</p>
+                    <p className="card-text">{project.description}</p>
                 </Dotdotdot>
-            <Button label="More" onClick={onClick}/>
+            </div>
+            <div>
+                <Button label="More" style={{margin: 0}} onClick={onClick}/>
+            </div>
         </div>
     </div>  
 );
