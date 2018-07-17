73058b0447ab69df5c3032d06e9543fdcadcc2c3
Sylvie Fiquet <sfiquetdev@gmail.com>
2018-07-17 23:27:00 +0200

    Adjust visual space in Features
    - remove bottom margin from paragraphs to balance top and bottom space
    - replace with column padding to separate items vertically when stacked
    - use a div to center the content vertically while keeping the icons
      and titles aligned when the paragraphs have different heights
    - add a container class for automatic horizontal spacing

--- src/components/organisms/About.js
+++ src/components/organisms/About.js
@@ -18,26 +18,30 @@ class About extends Component{
       {(!this.props.user) ?
         (
         <section className="about   ">
-           <div className="row features">
-               <div className="col-md-6 text-center">
+          <div className="features d-flex flex-column justify-content-center">
+            <div className="container py-0">
+              <div className="row">
+                <div className="col-md-6 text-center py-3">
                    <img className="about-icon" src={startup} alt=""/>
                     <h2 className="white-text mt-0">Discover projects</h2>
-                    <p className="white-text">Join exciting new projects, collaborate with other devs and finetune your dev skills!</p>
+                    <p className="white-text mb-0">Join exciting new projects, collaborate with other devs and finetune your dev skills!</p>
 
-               </div>
-               <div className="col-md-6 text-center">
+                </div>
+                <div className="col-md-6 text-center py-3">
                     <img className="about-icon" src={target} alt=""/>
                     <h2 className="white-text mt-0">Find team mates</h2>
-                    <p className="white-text">Need a little bit of help with your app? You've come to the right place! Find new teammates and finish your projects fast</p>
-               </div>
-            </div>
-            <div className="row join">
-                <div className="col-md-12 text-center my-5">
-                    <h2 className="display-4 white-text mt-0">Start your journey</h2>
-                    <p className="white-text">Build more projects, faster and together</p>
-                    <a href="/auth/github"><Button label="Join" /></a>
+                    <p className="white-text mb-0">Need a little bit of help with your app? You've come to the right place! Find new teammates and finish your projects fast</p>
                 </div>
+              </div>
             </div>
+          </div>
+          <div className="row join">
+            <div className="col-md-12 text-center my-5">
+                <h2 className="display-4 white-text mt-0">Start your journey</h2>
+                <p className="white-text">Build more projects, faster and together</p>
+                <a href="/auth/github"><Button label="Join" /></a>
+            </div>
+          </div>
         </section>
         ) : (
           null
